b4f1b74c01f570e5ad0c56134175b1548a8d93eb
Pablo Reyes <pablo@srtk.com>
2018-06-23 14:36:58 -0300

    small fix

--- php/scripts/php-cs-fixer.sh
+++ php/scripts/php-cs-fixer.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ./vendor/bin/php-cs-fixer fix \
-    --config=./resources/rules/php-cs-fixer.dist.php \
+    --config=./resources/rules/php-cs-fixer.php \
     --dry-run --stop-on-violation &&
 
 exit $?
