cb5d398c4c071bc020dad1a90b74113ad89b9fb1
Pablo Reyes <pablo@srtk.com>
2018-08-25 18:00:38 -0300

    Update/tslint phpcsfixer (#16)
    * update/tslint_phpcsfixer

--- php/rules/php-cs-fixer.dist.php
+++ php/rules/php-cs-fixer.dist.php
@@ -79,6 +79,18 @@ return PhpCsFixer\Config::create()
         'strict_comparison' => true,
         'no_useless_else' => true,
         'strict_param' => true,
+
+        // 2.13
+        'magic_method_casing' => true,
+        'fopen_flag_order' => true,
+        'combine_nested_dirname' => true,
+        'binary_operator_spaces' => true,
+        'no_alias_functions' => true,
+        'native_constant_invocation' => true,
+        'php_unit_method_casing' => true,
+        'implode_call' => true,
+        'no_unreachable_default_argument_value' => true,
+
         /*
         'ordered_class_elements' => [
             'use_trait',
