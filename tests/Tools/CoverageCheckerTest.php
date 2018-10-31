76d13c90873fb1f72762eb115cd3dcdac674aca2
Pablo Reyes <pablo@srtk.com>
2018-10-31 19:18:28 -0300

    coverage by folder (#21)

---
+++ tests/Tools/CoverageCheckerTest.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Copyright (C) 1997-2018 Reyesoft <info@reyesoft.com>.
+ *
+ * This file is part of LaravelJsonApi. LaravelJsonApi can not be copied and/or
+ * distributed without the express permission of Reyesoft
+ */
+
+declare(strict_types=1);
+
+namespace Tests\Tools;
+
+use PHPUnit\Framework\TestCase;
+
+class CoverageCheckerTest extends TestCase
+{
+    public function testCoverageReportByFolder(): void
+    {
+        $output = `php ./tools/coverage-checker.php ./tests/Tools/coverage-checker-files/clover.xml`;
+        $this->assertContains('for more information', $output);
+        $this->assertNotContains('which is below the accepted', $output);
+    }
+
+    public function testCoverageReportFail(): void
+    {
+        $output = `php ./tools/coverage-checker.php ./tests/Tools/coverage-checker-files/clover.xml 100`;
+        $this->assertContains('for more information', $output);
+        $this->assertContains('which is below the accepted', $output);
+    }
+
+    public function testCoverageReportOk(): void
+    {
+        $output = `php ./tools/coverage-checker.php ./tests/Tools/coverage-checker-files/clover.xml 55`;
+        $this->assertContains('for more information', $output);
+        $this->assertNotContains('which is below the accepted', $output);
+    }
+}
