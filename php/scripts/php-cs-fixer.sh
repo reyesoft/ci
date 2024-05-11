5adda4ff49afd2977527351310383e5b07d96edd
Pablo Reyes <pablo@reyesoft.com>
2024-05-11 17:10:21 -0300

    upgrade/php81 auto-commit

--- php/scripts/php-cs-fixer.sh
+++ php/scripts/php-cs-fixer.sh
@@ -2,6 +2,8 @@
 
 ./vendor/bin/php-cs-fixer fix \
     --config=./resources/rules/php-cs-fixer.php \
-    --dry-run --stop-on-violation &&
+    --allow-risky=yes \
+    --dry-run \
+    --stop-on-violation &&
 
 exit $?
